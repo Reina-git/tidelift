d439973ef8850965e4116da564a226d146a727b1
Reina Uhler <reinadu@gmail.comgit>
2021-08-26 12:22:19 -0700

    refactor into package.js

--- src/components/pages/AxiosPage.js
+++ src/components/pages/AxiosPage.js
@@ -1,52 +1,13 @@
-import React from "react";
-import axios from "axios";
 import Package from "../ui/Package";
 
-class AxiosPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      platform: "",
-      name: "",
-      description: "",
-      homepage: "",
-      keywords: [],
-    };
-  }
-  componentDidMount() {
-    axios
-      .get(
-        "https://libraries.io/api/npm/axios?api_key=ee71123bb7a1958beab2c188e3c9a842"
-      )
-      .then((res) => {
-        // handle success
-        const { platform, name, description, homepage, keywords } = res.data;
-        this.setState({
-          platform,
-          name,
-          description,
-          homepage,
-          keywords,
-        });
-        console.log(res);
-      })
-      .catch((error) => {
-        // handle error
-        console.log(error);
-      });
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <Package details={this.state} />
-          </div>
+export default function AxiosPage() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <Package project="axios" />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default AxiosPage;
--- src/components/ui/Package.js
+++ src/components/ui/Package.js
@@ -1,28 +1,83 @@
-export default function Package(props) {
-  const { platform, name, description, homepage, keywords } = props.details;
+import React from "react";
+import axios from "axios";
 
-  function getKeyWordString(words) {
-    return words.map((word, i, arr) => {
-      // if you are not on the last word
-      if (i < arr.length - 1) {
-        return word + ", ";
-      }
-      //  on the last word in the array
-      else return word;
-    });
+export default class Package extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      platform: "",
+      name: "",
+      description: "",
+      homepage: "",
+      license: "",
+      keywords: [],
+    };
+  }
+  componentDidMount() {
+    axios
+      .get(
+        `https://libraries.io/api/npm/${this.props.project}?api_key=ee71123bb7a1958beab2c188e3c9a842`
+      )
+      .then((res) => {
+        // handle success
+        const {
+          platform,
+          name,
+          description,
+          homepage,
+          keywords,
+          licenses,
+        } = res.data;
+        this.setState({
+          platform,
+          name,
+          description,
+          homepage,
+          keywords,
+          licenses,
+        });
+        console.log(res);
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      });
   }
 
-  return (
-    <div className="">
-      <h1 className="mt-5 mb-4">
-        {platform} / {name}
-      </h1>
-      <p>{description}</p>
+  render() {
+    const {
+      platform,
+      name,
+      description,
+      homepage,
+      keywords,
+      licenses,
+    } = this.state;
 
-      <p>Keywords: {getKeyWordString(keywords)}</p>
-      <a href={homepage} target="blank" rel="noopener noreferrer">
-        {homepage}
-      </a>
-    </div>
-  );
+    function getKeyWordString(words) {
+      return words.map((word, i, arr) => {
+        // if you are not on the last word
+        if (i < arr.length - 1) {
+          return word + ", ";
+        }
+        //  on the last word in the array
+        else return word;
+      });
+    }
+
+    return (
+      <div className="">
+        <h1 className="mt-5 mb-4">
+          {platform} / {name}
+        </h1>
+        <p>{description}</p>
+        <p>Licenses:{licenses}</p>
+
+        <p>Keywords: {getKeyWordString(keywords)}</p>
+        <a href={homepage} target="blank" rel="noopener noreferrer">
+          {homepage}
+        </a>
+      </div>
+    );
+  }
 }
